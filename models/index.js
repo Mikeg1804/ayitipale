aa756eaada2b1a3179c1ff8957096bdb85f94e7a
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-12-19 12:16:42 -0800

    updated author model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Blog = require("./Blog");
-const Author = require("./Author/index.js");
+const Author = require("./User");
 const Comment = require("./Comment");
 // const Likes = require("./Likes");
 // const ObsceneBlog = require("./ObsceneBlog");
