1616f6f90175c487659129d40078cb9db6c2b8ab
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-12-20 09:22:35 -0800

    UpdatedwithLauConnectionCodes

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,27 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+// when deployed on Heroku
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // localhost
+    sequelize = new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+module.exports = sequelize;
+
+
+
+
 // const Sequelize = require('sequelize');
 // require('dotenv').config();
 
@@ -20,29 +44,29 @@
 
 // module.exports = sequelize;
 
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
 
-let sequelize;
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  // For JawsDB on Heroku
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else if (process.env.DATABASE_URL) {
-  // For Heroku with ClearDB
-  sequelize = new Sequelize(process.env.DATABASE_URL);
-} else {
-  // For local development
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST || '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   // For JawsDB on Heroku
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else if (process.env.DATABASE_URL) {
+//   // For Heroku with ClearDB
+//   sequelize = new Sequelize(process.env.DATABASE_URL);
+// } else {
+//   // For local development
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: process.env.DB_HOST || '127.0.0.1',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
 
-module.exports = sequelize;
+// module.exports = sequelize;
